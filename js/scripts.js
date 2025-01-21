a82a1cb6711cc56c9fac101d461688bc1b9f44ac
Alberto Oliveira <alberto.olv00@gmail.com>
2025-01-21 17:01:47 -0300

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+import { adicionarItem } from "./adicionarItem.js";
+import { verificarListaComprados } from "./verificarListaComprados.js";
+
+const botaoSalvarItem = document.getElementById("adicionar-item");
+botaoSalvarItem.addEventListener("click", adicionarItem);
+
+const listaComprados = document.getElementById("lista-comprados");
+verificarListaComprados(listaComprados);
+
+// import { adicionarItem } from "./js/adicionarItem.js";
+// import { verificarListaComprados } from "./js/verificarListaComprados.js";
+
+// const botaoSalvarItem = document.getElementById("adicionar-item");
+// botaoSalvarItem.addEventListener("click", adicionarItem);
+
+// const listaComprados = document.getElementById("lista-comprados");
+// verificarListaComprados(listaComprados);
